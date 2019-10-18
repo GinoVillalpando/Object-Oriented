60816a066aefcfdd5b15dd57de83cb99b9d1e053
Gino Villalpando <grieveous@live.com>
2019-10-17 21:19:05 -0600

    I BEAT THE BOSS!!!

--- php/Classes/ValidateDate.php
+++ php/Classes/ValidateDate.php
@@ -6,8 +6,6 @@ namespace GinoVillalpando\ObjectOriented;
  * This trait will inject a private method to validate a mySQL style date (e.g., 2016-01-15 15:32:48.643216). It will
  * convert a string representation to a DateTime object or throw an exception.
  *
- * @author Dylan McDonald <dmcdonald21@cnm.edu>
- * @version 4.0.1
  **/
 trait ValidateDate {
 	/**
--- php/Classes/ValidateUuid.php
+++ php/Classes/ValidateUuid.php
@@ -15,7 +15,7 @@ use \Ramsey\Uuid\Uuid;
  * 2. binary string (16 bytes)
  * 3. Ramsey\Uuid\Uuid object
  *
- * @author Dylan McDonald <dmcdonald21@cnm.edu>
+ *
  * @package Edu\Cnm\Misquote
  **/
 trait ValidateUuid {
